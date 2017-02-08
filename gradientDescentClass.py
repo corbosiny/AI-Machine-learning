de13755ecbe4f29e99bf6227a547e13022926296
Corey Hulse <cohulse@asu.edu>
2017-02-08 10:13:41 -0500

    added normal equation method

--- gradientDescentClass.py
+++ gradientDescentClass.py
@@ -1,3 +1,5 @@
+import matrix
+
 class gradientDescent():
 
     def __init__(self, trainingData, weights, alpha, featureScaling = False, dp = None):
@@ -96,11 +98,14 @@ class gradientDescent():
 
         return tempWeights
     
-    def fit(self, numSteps):
-        for i in range(numSteps):
-            self.weights = self.adjustWeights()
-        self.cleanUp()
-        return self.weights
+    def fit(self, numSteps, normal = False):
+        if not normal:
+            for i in range(numSteps):
+                self.weights = self.adjustWeights()
+            self.cleanUp()
+            return self.weights
+        else:
+            pass
 
     def predict(self, inputs, featureScale = None):
         if featureScale == None:
@@ -113,6 +118,13 @@ class gradientDescent():
             output += weight * inputs[i]
         return output
 
+    def normalEquation(self):
+        inputMatrix = matrix.Matrix(self.inputs)
+        outputMatrix = matrix.Matrix([[y] for y in self.outputs])
+        inverse = (inputMatrix.transpose() * inputMatrix).inverse()
+        weights = inverse * inputMatrix.transpose() * outputMatrix
+        return weights
+    
     def __str__(self):
         string = 'Weights: '
         for weight in self.weights:
@@ -122,19 +134,21 @@ class gradientDescent():
         string += '\nAlpha: ' + str(self.alpha)
         string += '\nFeature Scaling: ' + str(self.featureScaling)
         return string
-    
+
+
 if __name__ == "__main__":
     trainingData = [[[1,4], 110],[[1,5], 135],[[1,6], 160],[[1,7], 185],[[1,8], 210]]
     weights = [-5,-8]
     
-    grad = gradientDescent(trainingData, weights, .1, True, 3)
-    print(grad.fit(50000))
-    print()
-    for i in range(len(trainingData)):
-        print(grad.inputs[i])
-        print(grad.outputs[i])
-        print(grad.predict(trainingData[i][0], False))
-        print()
-    print('Final Test:')
-    print(9 * 25 + 10)
-    print(grad.predict([1,9]))
+    grad = gradientDescent(trainingData, weights, .1, False, 3)
+##    print(grad.fit(500))
+##    print()
+##    for i in range(len(trainingData)):
+##        print(grad.inputs[i])
+##        print(grad.outputs[i])
+##        print(grad.predict(trainingData[i][0], False))
+##        print()
+##    print('Final Test:')
+##    print(9 * 25 + 10)
+##    print(grad.predict([1,9]))
+    print(grad.normalEquation())
