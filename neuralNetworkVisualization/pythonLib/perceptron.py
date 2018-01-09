1f29c066699e9e444f7c8ebbd4a5f116eaedd6b5
Corey Hulse <cohulse@asu.edu>
2018-01-09 15:53:13 -0600

    added new cost functions

--- neuralNetworkVisualization/pythonLib/perceptron.py
+++ neuralNetworkVisualization/pythonLib/perceptron.py
@@ -3,18 +3,55 @@ import numpy as np
 
 class Perceptron():
     
-    def __init__(self, weights):
+    def __init__(self, weights, bias, activationType = "sigmoid"):
         self.weights = np.array([weight for weight in weights])
+        self.bias = bias
+        self.activationFunction = initActivationFunction(activationType)
 
+    def initActivationFunction(self, activationType):
+        activationType = activationType.lower()
+        if activationType == "sigmoid":
+            return Perceptron.sigmoidFunction
+        elif activationType == "relu":
+            return Perceptron.reluFunction
+        elif activationType == "identity":
+            return Perceptron.identity
+        elif activationType == "binary":
+            return Perceptron.binary
+        elif activationType == "tangent":
+            return Perceptron.tanH
+        elif activationType = "arctan":
+            return Perceptron.arctan
+        
     def calculateOutput(self, inputs):
-        weightedSum = self.weightAndSumInputs(inputs)
-        return self.activationFunction(weightedSum)
+        weightedSum = sum(inputs * self.weights)
+        biasedSum = weightedSum - self.bias
+        return self.activationFunction(weightedSum)       
 
-    def weightAndSumInputs(self, inputs):
-        return sum(inputs * self.weights)        
 
-    def activationFunction(self, inpt):
+    def sigmoidFunction(inpt):
         return math.exp(inpt) / (math.exp(inpt) + 1)
 
+    def reluFunction(inpt):
+        if inpt > 0:
+            return inpt
+        else:
+            return 0
 
-    
+    def identity(inpt):
+        return inpt
+
+    def binary(inpt):
+        if inpt > 0:
+            return 1
+        else:
+            return 0
+
+    def tanH(inpt):
+        return (2 / (1 + math.exp(-2 * inpt))) - 1
+
+    def arctan(inpt):
+        return math.atan(inpt)
+
+if __name__ == "__main__":
+    pass
