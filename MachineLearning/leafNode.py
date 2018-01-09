b7ec21335459e60d3a6a384f0189452415ac80bb
Corey Hulse <cohulse@asu.edu>
2018-01-08 21:29:39 -0600

    finished up two of the three support classes for our desicion tree

---
+++ MachineLearning/leafNode.py
@@ -0,0 +1,40 @@
+from __future__ import division
+
+class Leaf():
+
+    def __init__(self, data):
+        self.data = data
+
+    def predict(self):
+        labels = set([point[-1] for point in self.data])
+
+        if len(labels) == 0:
+            return labels[0]
+        else:
+            return self.predictFromLabels(labels)
+
+    def predictFromLabels(self, labels):
+        dataLabels = [point[-1] for point in self.data]
+        counts = {label : dataLabels.count(label) for label in labels}
+
+        probabilities = {label : counts[label] / len(dataLabels) for label in labels}
+
+        bestLabel = Leaf.findMaxLabel(probabilities)
+        
+        return bestLabel
+
+    def findMaxLabel(probabilities):
+        bestLabel, bestProbability = None, 0
+
+        for label in probabilities:
+            if probabilities[label] > bestProbability:
+                bestLabel = label
+                bestProbability = probabilities[label]
+
+        return bestLabel
+
+
+if __name__ == "__main__":
+    testData = [[1, 1], [5, 1], [2, 1], [2, 1], [3, 0], [1, 0]]
+    leaf = Leaf(testData)
+    print(leaf.predict())
--- MachineLearning/question.py
+++ MachineLearning/question.py
@@ -5,14 +5,26 @@ class Question():
         self.columnNumber = columnNumber
         self.valueToCompare = valueToCompare
 
-    def match(self, ):
-        if is_numeric()
+    def match(self, rowVal):
+        if Question.isNumeric(self.valueToCompare):
+            return rowVal >= self.valueToCompare
+        else:
+            return rowVal == self.valueToCompare
         
     def __repr__(self):
         if Question.isNumeric(self.valueToCompare):
-
+            return "Is feature #%d >= %d ?" % (self.columnNumber, self.valueToCompare)
         else:
-
+            return "Is feature #%d == %s ?" % (self.columnNumber, self.valueToCompare)
+            
         
     def isNumeric(value):
         return isinstance(value, int) or isinstance(value, float)
+
+
+if __name__ == "__main__":
+    question = Question(3, 5)
+    print(question)
+    print(question.match(5))
+    print(question.match(4))
+    print(question.match(6))
