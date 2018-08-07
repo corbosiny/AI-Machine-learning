30fa1f5f1c5887732cd469f4f3cf050fa0ea1388
corbosiny <cohulse@asu.edu>
2018-08-06 19:48:36 -0500

    Added testing for the connect4Board object

--- Connect4AI/connect4.py
+++ Connect4AI/connect4.py
@@ -111,8 +111,85 @@ class Connect4Game(threading.Thread):
     
     def __str__(self):         
         return str(self.board)
+
+
+def checkIfInitialBoardVariablesAreValid(game):
+    assert(game.boardHeight == DEFAULT_BOARD_HEIGHT)
+    assert(game.boardLength == DEFAULT_BOARD_LENGTH)
+    assert(game.players == [])
+    assert(game.turn == 0)
+    assert(game.numMoves == 0)
+    assert(game.gameOver == False)
+    assert(game.winner == None)
+    assert(game.displayGame == False)
+    assert(game.board != None)
     
-if __name__ == "__main__":              
+    return True
+
+def checkAddingPlayers(game):
+    import connect4PlayerShell
+    player1 = connect4PlayerShell.Connect4PlayerShell(0)
+    player2 = connect4PlayerShell.Connect4PlayerShell(1)
+
+    player1.joinGame(game)
+    assert(player1.playerSymbol == game.playerSymbols[0])
+    assert(game.players[0] == player1)
+
+    player2.joinGame(game)
+    assert(player2.playerSymbol == game.playerSymbols[1])
+    assert(game.players[1] == player2)
+
+    return True
+
+def checkRemovingPlayers(game):
+    game.removePlayersFromGame()
+    assert(game.players == [])
+
+    return True
+    
+
+def checkIfGameRan(game):
+    game.start()
+    assert(game.winner == None)
+
+    import connect4PlayerShell
+    player1 = connect4PlayerShell.Connect4PlayerShell(0)
+    player2 = connect4PlayerShell.Connect4PlayerShell(1)
+
+    game.addPlayer(player1)
+    game.addPlayer(player2)
+
+    while game.winner == None:
+        pass
+
+    assert(game.numMoves > 7)
+    assert(game.winner == game.players[not game.turn])
+    assert(game.winner.wins == 1)
+    assert(game.gameOver == True)
+
+    return True
+
+def checkCleanUp(game):
+    game.prepareForNewGame()
+    assert(checkIfInitialBoardVariablesAreValid(game) == True)
+
+    return True
+    
+
+def runGameDiagnostics():
+    newGame = Connect4Game()
+    assert(checkIfInitialBoardVariablesAreValid(newGame) == True)
+    assert(checkAddingPlayers(newGame) == True)
+    assert(checkRemovingPlayers(newGame) == True)
+    assert(checkIfGameRan(newGame) == True)
+    assert(checkCleanUp(newGame) == True)
+    
+    return True
+
+if __name__ == "__main__":
+    assert(runGameDiagnostics() == True)
+    print("Initial Diagnostics Passed!\n\n")
+    
     newGame = Connect4Game(True)
 
     HumanPlayer = Connect4HumanPlayer(1)
